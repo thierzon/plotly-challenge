b1603b03c70ed05149245c462f196d0445289140
thierzon <thierryzonneveld@gmail.com>
2021-04-16 13:21:05 +0800

    app.js working

--- static/js/app.js
+++ static/js/app.js
@@ -1,60 +1,114 @@
-// Use d3.json() to fetch data from JSON file
-d3.json("samples.json").then(function(data) {
-  var samples = Object.values(data.samples);
-  var names = Object.values(data.names);
-  var metadata = Object.values(data.metadata);
+// select html elements using d3
+var dropdown = d3.select("#selDataset");
+var demotable = d3.select("#sample-metadata");
+var barchart = d3.select("#bar");
+var bubblechart = d3.select("#bubble");
+var gaugechart = d3.select("#gauge");
+
+// plot data at page initialization
+function init() {
+  plotData();
+};
+
+// plot data function to update page
+function plotData() {
+
+  // read in data from json file using d3
+  d3.json("samples.json").then(function(data) {
+    var samples = Object.values(data.samples);
+    var names = Object.values(data.names);
+    var metadata = Object.values(data.metadata);
   
-  // select and populate dropdown menu
-  var dropdown = d3.select("#selDataset");
-  names.forEach((item) => {
-    var row = dropdown.append("option")
-    .attr("value", item);
-    row.text(item);
+    // add test samples to dropdown menu
+    names.forEach((item) => {
+      var row = dropdown.append("option").attr("value", item);
+      row.text(item);
+    });
+
+    // populate demo table
+    var tableBody = demotable.append("tbody");
+    var inputValue = dropdown.property("value");
+
+    var filteredData = metadata.filter(item => item.id == inputValue);
+    var filteredSamples = samples.filter(item => item.id == inputValue);
+
+    filteredData.forEach((item) => {
+      row = tableBody.append("tr");
+      Object.entries(item).forEach(value => {
+          cell = row.append("tr");
+          cell.text("");
+          cell.text(`${value[0]}: ${value[1]}`);
+      });
+    });
+    
+    // slice the sample data to create horizontal bar chart
+    var barSampleValues = filteredSamples[0].sample_values.slice(0,10).reverse();
+    var barOTUs = filteredSamples[0].otu_ids.slice(0,10).reverse().map(data => `OTU ` + data);
+    var barLabels = filteredSamples[0].otu_labels.slice(0,10).reverse();
+
+    // prepare and plot horizontal bar chart
+    var trace1 = {
+      x: barSampleValues,
+      y: barOTUs,
+      text: barLabels,
+      type: "bar",
+      orientation: "h"
+    };
+    var bardata = [trace1];
+
+    var barlayout = {
+        title: "Top 10 OTUs in Sample",
+        xaxis: {title: "Prevalence in Sample"},
+        yaxis: {title: "OTU ID Number"}  
+    };
+    Plotly.newPlot("bar", bardata, barlayout);
+
+    // select sample data to create bubble chart
+    var bubbleSampleValues = filteredSamples[0].sample_values;
+    var bubbleOTUs = filteredSamples[0].otu_ids;
+    var bubbleLabels = filteredSamples[0].otu_labels;
+
+    // prepare and plot bubble chart
+    var trace2 = {
+      x: bubbleOTUs,
+      y: bubbleSampleValues,
+      mode: 'markers',
+      marker: {
+        size: bubbleSampleValues,
+        color: bubbleOTUs, 
+        text: bubbleLabels
+      }
+    };
+    var bubbledata = [trace2];
+
+    var bubblelayout = {
+        xaxis: {title: "OTU ID Number"},
+        yaxis: {title: "Sample"}  
+    };
+    Plotly.newPlot("bubble", bubbledata, bubblelayout);
+
   });
-  
-  // select and populate demographics table
-  var demodata = d3.select("#sample-metadata");
+};
 
-  demodata.html("")
-  
-  var inputElement = d3.select("#selDataset");
-  var tableBody = demodata.append("tbody");
-  var inputValue = inputElement.property("value");
+// reset function
+function resetData() {
 
-  var filteredData = metadata.filter(item => item.id == inputValue);
-  var filteredSamples = samples.filter(item => item.id == inputValue);
+  // reset table and charts
+  demotable.html("");
+  barchart.html("");
+  bubblechart.html("");
+  gaugechart.html("");
+};
 
-  filteredData.forEach((item) => {
-    row = tableBody.append("tr");
-    Object.entries(item).forEach(value => {
-        cell = row.append("tr");
-        cell.text("");
-        cell.text(`${value[0]}: ${value[1]}`);
-    })
-  });
+function optionChanged() {
 
-  var slicedSampleValues = filteredSamples[0].sample_values.slice(0,10).reverse();
-  var slicedOTUs = filteredSamples[0].otu_ids.slice(0,10).reverse().map(data => `OTU ` + data);
-  var slicedLabels = filteredSamples[0].otu_labels.slice(0,10).reverse();
+  resetData();
+  plotData();
 
-  var trace1 = {
-    x: slicedSampleValues,
-    y: slicedOTUs,
-    text: slicedLabels,
-    type: "bar",
-    orientation: "h"
-  };
-  var bardata = [trace1];
+}; 
 
-  var barlayout = {
-      title: "Top 10 OTUs in Sample",
-      xaxis: {title: "Prevalence in Sample"},
-      yaxis: {title: "OTU ID Number"}  
-  };
-  Plotly.newPlot("bar", bardata, barlayout);
+// when dropdown menu changes run the optionChanged function
+d3.selectAll("#selDataset").on("change", optionChanged);
 
-
-  dropdown.on("change", dropdownchange);
-
-});
-
+// populate page with data of first test sample
+init();
